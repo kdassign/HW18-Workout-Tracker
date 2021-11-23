5c15dbb59ca823b8cc8bc3063f22251455aa4140
Kelsey Duru <kdassignments@gmail.com>
2021-11-23 15:16:22 -0800

    now I need to see if everything works

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,19 +1,17 @@
 const router = require("express").Router();
 const path = require('path');
 
-// GET /exercise
 router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, '/../public/exercise.html'));
 });
 
-// GET /exercise?
 router.get("/exercise?", (req, res) => {
-  // location.search = "?id=" + workout._id;
+
 
   res.sendFile(path.join(__dirname, '/../public/exercise.html'));
 });
 
-// GET /stats
+
 router.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname, '/../public/stats.html'));
 });
