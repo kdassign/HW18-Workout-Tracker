95b8c6f9e9cc9124f5c7a45a0413bf690560e786
Kelsey Duru <kdassignments@gmail.com>
2021-11-28 20:13:32 -0800

    work

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/hw18", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Hw18", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
